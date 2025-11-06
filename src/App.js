b8094749acd10648169ec43baac37e3f1ae5170c
ilunchoe <ilunchoe@gmail.com>
2025-11-06 19:28:10 +0900

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [stateCount, setStateCount] = useState(0);
+  const refCount = useRef(0);
+  //변수처럼 값을 저장할 필요가 있는 변수, 단 ui에 보일 필요가 없는 얘들
+  console.log("render");
+  let count = 0;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div> state: {stateCount} </div>
+      <div> 변수 : {count}</div>
+      <div> ref: {refCount.current}</div>
+      <button onClick={() => setStateCount((prev) => prev + 1)}>
+        state up
+      </button>
+
+      <button
+        onClick={() => {
+          count++;
+          console.log("변수", count);
+        }}
+      >
+        변수 up
+      </button>
+      <button
+        onClick={() => {
+          refCount.current++;
+          console.log("ref", refCount);
+        }}
+      >
+        ref up
+      </button>
     </div>
   );
 }
